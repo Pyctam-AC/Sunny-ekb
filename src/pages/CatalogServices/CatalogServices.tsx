dbe930ad8f8bcad2881a160c77ac4d73fb5c3360
Rustam Sultangaliev <sultangaliev-ru@yandex.ru>
2024-01-09 03:02:08 +0500

    fix CatalogPage & feat BackButton

--- src/pages/CatalogServices/CatalogServices.tsx
+++ src/pages/CatalogServices/CatalogServices.tsx
@@ -2,12 +2,14 @@ import { FC } from "react";
 //import styles from "./CatalogServices.module.scss";
 import LayoutMain from "../../Layout/LayoutMain";
 import ServicesCards from "../../components/ServicesCards/ServicesCards";
+import BackButton from "../../components/BackButton/BackButton";
 
 
 const CatalogServices:FC = () => {
 
   return (
     <LayoutMain>
+      <BackButton />
       <ServicesCards />
     </LayoutMain>
 
